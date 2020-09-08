e6c1d95daa95230c1e10214c5eb8a1f5680f01f3
unknown <luiz.gustavon@hotmail.com>
2020-09-07 22:35:35 -0300

    general style and modal script

--- js/app.js
+++ js/app.js
@@ -1,10 +1,20 @@
 const gallery = document.querySelector('.gallery');
 const modal = document.querySelector('.modal');
+const modalItems = document.querySelector('.modal-items');
+const closeBtn = document.querySelector('.fa-times');
 
 gallery.addEventListener('click', ModalHandler);
+closeBtn.addEventListener('click', CloseModal);
 
 function ModalHandler(e){
     if(e.target.classList.contains('gallery-item')){
-        console.log('teste');
+        let srcImg = e.target.src;
+        modalItems.firstElementChild.src = srcImg;
+        
+        modal.style.display = 'block';
     }
+}
+
+function CloseModal(){
+    modal.style.display = 'none';
 }
